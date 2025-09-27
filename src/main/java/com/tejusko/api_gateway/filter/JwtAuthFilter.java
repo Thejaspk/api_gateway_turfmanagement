6376fb9be3294fda4bb01a4ce8e2c2900d786303
Tejas PK <thejaspk@kran.co.in>
2025-09-27 18:52:09 +0530

    first commit

---
+++ src/main/java/com/tejusko/api_gateway/filter/JwtAuthFilter.java
@@ -0,0 +1,62 @@
+package com.tejusko.api_gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+@Component
+public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public JwtAuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return this.onError(exchange, "Missing Authorization Header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = authHeader.substring(7);
+            try {
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                // Optionally forward username to backend
+                exchange = exchange.mutate()
+                        .request(r -> r.headers(h -> h.set("X-User", claims.getSubject())))
+                        .build();
+
+            } catch (Exception e) {
+                return this.onError(exchange, "Invalid Token", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {}
+}
